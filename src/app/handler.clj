1f10fec79673ba2b11bc00327c58a779ffd92d99
vachi <vachichng@gmail.com>
2020-07-02 11:53:28 +0200

    add service post route and fallback error route

--- src/app/handler.clj
+++ src/app/handler.clj
@@ -2,10 +2,12 @@
   (:require
     [compojure.core :refer :all]
     [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
-    [ring.middleware.json :refer [wrap-json-body wrap-json-response]]))
+    [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+    [app.service :as service]))
 
 (defroutes app-routes
-  (ANY "*" [] (fn [_] {:status 200 :body "Hello World!" :headers {"Content-Type" "application/json "}})))
+  (POST "/" [] (fn [req] {:status 200 :body (service/compute (-> req :body)) :headers {"Content-Type" "application/json"}}))
+  (ANY "*" [] (fn [_] {:status 404 :body {:error "not found"} :headers {"Content-Type" "application/json "}})))
 
 (def app-handler
   (-> app-routes
