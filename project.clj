fd6efbb8d10d8b581d8e96f323d4363055e7fcaf
vachi <vachichng@gmail.com>
2020-07-02 12:48:00 +0200

    add http client for integration tests

--- project.clj
+++ project.clj
@@ -12,4 +12,5 @@
   :plugins []
   :main app.core
   :profiles
-  {:dev {:dependencies [[expectations/clojure-test "1.2.1"]]}})
+  {:dev {:dependencies [[expectations/clojure-test "1.2.1"]
+                        [clj-http "3.10.1"]]}})
